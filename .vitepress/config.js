705ab6b4b783d21bf618362e3d5795358f309814
Estrella-Explore <151703168+Estrella-Explore@users.noreply.github.com>
2025-08-03 22:37:52 +0800

    feat:Add evidence (#5)
    * update(assets): add origin paper
    
    * fix(.github/workflows): the correct name should be `.github/workflow(s)`
    
    * fix(.github/workflows/markdown_lint.yml): fix `markdownlnt` command
    
    * tiny(readme.md): fix markdown format according to `markdownlnt`
    
    * fix(readme.md): warning by `pymarkdownlnt` MD009
    
    * feat(.github/workflows/markdown_lint.yml): add ci/cd cache
    
    * feat(README.md): update readme
    
    * format(readme.md): format `readme.md`
    
    * feat: ignore pymarkdownlnt warnings
    
    * feat(disclosure/timeline.md): 杨景媛诬告案及学术不端事件过程梳理
    
    * feat: 进度列表
    
    * Add Vitepress and Github Pages(#4)
    
    * feat(vitepress): add github pages for the repo
    
    * update(deploy.yml): update upload-pages-artifact@v4
    
    * update: deprecated
    
    * update: deploy on pr
    
    * typo(guide.yml)
    
    * update: deploy on all pr
    
    * update `update-artifact`
    
    * feat: npm
    
    * update and add preview
    
    * update: change name to github pages to fit `actions/deploy-pages`
    
    * fix error
    
    * fix dir location
    
    * feat: ignore dead links
    
    * fix
    
    * fix: fix path
    
    * resolve conflicts

---
+++ .vitepress/config.js
@@ -0,0 +1,11 @@
+// .vitepress/config.js
+export default {
+  ignoreDeadLinks: true,
+  title: '杨景媛学术不端',
+  description: '杨景媛硕士论文学术不端事件一条龙',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' }
+    ]
+  }
+}
